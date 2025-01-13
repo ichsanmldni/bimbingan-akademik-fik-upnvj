41e69f7f8dc1317457c06fef0a2cab5e16a51650
Ichsan Maldini Hamid <ichsan225@gmail.com>
2025-01-14 01:55:13 +0700

    fixing

--- src/components/ui/TrashButton.tsx
+++ src/components/ui/TrashButton.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import trashIcon from "../../assets/images/trash-icon.png";
 
-const TrashButton: React.FC<any> = ({ onClick, className }) => {
+const TrashButton = ({ onClick, className }) => {
   return (
     <button className={className} onClick={onClick}>
       <Image src={trashIcon} className="size-4" alt="Edit Icon" />
